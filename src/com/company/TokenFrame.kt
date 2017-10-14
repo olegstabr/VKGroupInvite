f911f4a3637d67be62791b9bb0f7130174efbfc7
olegstabr <olegstabr@gmail.com>
2017-10-14 07:40:43 +0700

    Fix TokenFrame GUI

--- src/com/company/TokenFrame.kt
+++ src/com/company/TokenFrame.kt
@@ -12,36 +12,29 @@ class TokenFrame : JFrame() {
 
     init {
         title = "Авторизация"
-        size = Dimension(500, 120)
+        size = Dimension(450, 100)
         defaultCloseOperation = JFrame.EXIT_ON_CLOSE
-        layout = BoxLayout(contentPane, BoxLayout.Y_AXIS)
+        layout = GridLayout(3, 1)
         setLocationRelativeTo(null)
         isVisible = true
 
         label.text = "Введите токен"
         label.size = Dimension(400, 20)
-        label.alignmentX = Component.CENTER_ALIGNMENT
         label.font = Font("SansSerif", Font.BOLD, 16)
 
         textField.size = Dimension(400, 20)
-        textField.alignmentX = Component.CENTER_ALIGNMENT
         textField.font = Font("SansSerif", Font.PLAIN, 20)
 
         button.text = "ОК"
         button.size = Dimension(100, 20)
-        button.alignmentX = Component.CENTER_ALIGNMENT
         button.font = Font("SansSerif", Font.PLAIN, 14)
         button.addActionListener {
             MainFrame(textField.text)
             dispose()
         }
 
-        contentPane.add(Box.createRigidArea(Dimension(0, 5)))
         contentPane.add(label)
-        contentPane.add(Box.createRigidArea(Dimension(0, 5)))
         contentPane.add(textField)
-        contentPane.add(Box.createRigidArea(Dimension(0, 5)))
         contentPane.add(button)
-        contentPane.add(Box.createRigidArea(Dimension(0, 5)))
     }
 }
